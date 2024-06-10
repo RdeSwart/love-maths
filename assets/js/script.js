bb2d10a39413d994379cc6389bdb538d14844c94
Ruth de Swart <ruthdeswart@outlook.ie>
2024-06-10 09:31:44 +0000

    feat: Add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,10 +30,16 @@ function runGame(gameType) {
 
     if(gameType === "addition") {
         displayAdditionQuestion(num1, num2);
-    } else {
+    } else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
+    }
+    
+    else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;    
     }
+
+    
 }
 
 /**
@@ -68,7 +74,11 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
-    } else {
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
+    }
+    
+    else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}.Aborting!`;
     }
@@ -109,7 +119,7 @@ document.getElementById('operator').textContent = "-";
 function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
 document.getElementById('operand2').textContent = operand2;
-document.getElementById('operator').textContent = "*";
+document.getElementById('operator').textContent = "x";
     
 }
 
