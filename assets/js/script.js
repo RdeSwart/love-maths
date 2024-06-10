952002baa6cb73c14f81868d44c155219ffaed9e
Ruth de Swart <ruthdeswart@outlook.ie>
2024-06-10 10:57:01 +0000

    add division game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,8 @@ function runGame(gameType) {
         displaySubtractQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivideQuestion(num1, num2);
     }
     
     else {
@@ -90,6 +92,8 @@ function calculateCorrectAnswer() {
         return [operand1 - operand2, "subtract"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
+    } else if (operator === "/") {
+        return [Math.floor(operand1 / operand2), "division"];
     }
     
     else {
@@ -138,8 +142,9 @@ document.getElementById('operator').textContent = "x";
 }
 
 function displayDivideQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = operand1 * operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
 document.getElementById('operator').textContent = "/";
     
 }
